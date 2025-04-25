6b1c59f0cdafcc9b12407e28d7ffad6045cb3f11
Amanda Nielsen <amandasn202@gmail.com>
2025-04-25 15:27:50 +0200

    css roots and map-structure

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import './landingpage.css';
+
+function LandingPage() {
+  return (
+	<section className="landingpage">
+		<img src="./src/assets/images/logo.png" alt="logo with confetti" />
+		<h1 className="title__landing">Where It's @</h1>
+		<h2 className="subtitle__landing">Ticketing made easy</h2>
+	</section>
+  )
+}
+
+export default LandingPage;
